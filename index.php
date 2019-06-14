b590e8caad2c4d44e57a42172c4c786bb8a22262
rylebrun <r.lebrun@cen-aquitaine.fr>
2019-06-14 12:29:37 +0000

    Add simple galery filter

--- index.php
+++ index.php
@@ -9,7 +9,13 @@ include("system_header.php");
 
 <h1>Galerie photo</h1>
 
-<p class="breadcrumb"><a href="/">Accueil</a> &gt; Galerie</p>
+<p class="breadcrumb"><a href="/">Accueil</a> &gt; <a href="<?php echo $gallery_url;?>"> Galerie </a></p>
+
+<!-- Add gallery filter -->
+<form action="<?php echo $gallery_url;?>">
+    <p class="breadcrumb" style="text-align: right; padding-right: 30px"> Rechercher une galerie <input type="text" id="filter" name="filter"><input type="submit" value="Filtrer"></p>
+<!-- End gallery filter -->
+</form>
 
 <?php if(count($categories_array)<=0){?>
 <p>Il n'y a pas encore de catégories, créez une catégorie pour téléverser des photos.</p>
@@ -25,21 +31,36 @@ include("system_header.php");
     
     	<?php 
 		$category_thumbnail = $gallery_url."/layout/pixel.gif";
+
 		if(file_exists('files/'.$photo_category.'/thumbnail.jpg')){
 			$category_thumbnail = $gallery_url.'/'.rawurlencode($photo_category).'.jpg';
 		}
-		
+
 		$category_url = $gallery_url.'/'.rawurlencode($photo_category);
-		?>
-        
-  		<span class="category_thumbnail_span" style="width:<?php echo $settings_thumbnail_width;?>px; height:<?php echo $settings_thumbnail_height+20;?>px;">
-        <a class="category_thumbnail_image" href="<?php echo $category_url;?>" style="width:<?php echo $settings_thumbnail_width;?>px; height:<?php echo $settings_thumbnail_height;?>px; background-image:url('<?php echo $gallery_url;?>/layout/lens_48x48.png');" title="<?php echo htmlentities(ucwords(str_replace('-', ' ', $photo_category)), ENT_QUOTES, "UTF-8");?>">
-        <img src="<?php echo $category_thumbnail;?>" width="<?php echo $settings_thumbnail_width;?>" height="<?php echo $settings_thumbnail_height;?>" alt="<?php echo htmlentities(ucwords(str_replace('-', ' ', $photo_category)), ENT_QUOTES, "UTF-8");?>" />
-        </a>
-        <a class="category_thumbnail_title" href="<?php echo $category_url;?>" title="<?php echo htmlentities(ucwords(str_replace('-', ' ', $photo_category)), ENT_QUOTES, "UTF-8");?>">
-        <?php echo htmlentities(str_replace('-',' ', truncate_by_letters($photo_category, 16, '..')), ENT_QUOTES, "UTF-8");?> (<?php echo count($photos_array);?>)
-        </a>
-        </span>
+
+            if($_GET['filter'] && preg_match("/{$_GET['filter']}/i", $photo_category)){?>
+
+                <span class="category_thumbnail_span" style="width:<?php echo $settings_thumbnail_width;?>px; height:<?php echo $settings_thumbnail_height+20;?>px;">
+                <a class="category_thumbnail_image" href="<?php echo $category_url;?>" style="width:<?php echo $settings_thumbnail_width;?>px; height:<?php echo $settings_thumbnail_height;?>px; background-image:url('<?php echo $gallery_url;?>/layout/lens_48x48.png');" title="<?php echo htmlentities(ucwords(str_replace('-', ' ', $photo_category)), ENT_QUOTES, "UTF-8");?>">
+                <img src="<?php echo $category_thumbnail;?>" width="<?php echo $settings_thumbnail_width;?>" height="<?php echo $settings_thumbnail_height;?>" alt="<?php echo htmlentities(ucwords(str_replace('-', ' ', $photo_category)), ENT_QUOTES, "UTF-8");?>" />
+                </a>
+                <a class="category_thumbnail_title" href="<?php echo $category_url;?>" title="<?php echo htmlentities(ucwords(str_replace('-', ' ', $photo_category)), ENT_QUOTES, "UTF-8");?>">
+                <?php echo htmlentities(str_replace('-',' ', truncate_by_letters($photo_category, 16, '..')), ENT_QUOTES, "UTF-8");?> (<?php echo count($photos_array);?>)
+                </a>
+                </span>
+
+            <?php } elseif(!isset($_GET['filter']))  { ?>
+
+                <span class="category_thumbnail_span" style="width:<?php echo $settings_thumbnail_width;?>px; height:<?php echo $settings_thumbnail_height+20;?>px;">
+                <a class="category_thumbnail_image" href="<?php echo $category_url;?>" style="width:<?php echo $settings_thumbnail_width;?>px; height:<?php echo $settings_thumbnail_height;?>px; background-image:url('<?php echo $gallery_url;?>/layout/lens_48x48.png');" title="<?php echo htmlentities(ucwords(str_replace('-', ' ', $photo_category)), ENT_QUOTES, "UTF-8");?>">
+                <img src="<?php echo $category_thumbnail;?>" width="<?php echo $settings_thumbnail_width;?>" height="<?php echo $settings_thumbnail_height;?>" alt="<?php echo htmlentities(ucwords(str_replace('-', ' ', $photo_category)), ENT_QUOTES, "UTF-8");?>" />
+                </a>
+                <a class="category_thumbnail_title" href="<?php echo $category_url;?>" title="<?php echo htmlentities(ucwords(str_replace('-', ' ', $photo_category)), ENT_QUOTES, "UTF-8");?>">
+                <?php echo htmlentities(str_replace('-',' ', truncate_by_letters($photo_category, 16, '..')), ENT_QUOTES, "UTF-8");?> (<?php echo count($photos_array);?>)
+                </a>
+                </span>
+
+            <?php } ?>
     
 
 	<?php } ?>
